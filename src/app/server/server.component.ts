a0084a5e9ec9db8c4f3e1cfff7d595e2c174e067
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-05-15 14:19:14 +0200

    create server component

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  standalone: true,
+  imports: [],
+  templateUrl: './server.component.html',
+  styleUrl: './server.component.scss'
+})
+export class ServerComponent {
+
+}
